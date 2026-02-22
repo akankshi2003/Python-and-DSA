e854b83f05467ef030196cc9b57483c1b1fdfd2a
akankshi2003 <akankshi2403@gmail.com>
2026-02-22 23:01:23 +0530

    changed fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,15 +4,21 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        list = []
+        result = []
         for i in range(1, n+1):
             if i%3==0 and i%5==0:
-                list.append("Fizzbuzz")
+                result.append("Fizzbuzz")
             elif i%3==0:
-                list.append("Fizz")
+                result.append("Fizz")
             elif i%5==0:
-                list.append("Buzz")
+                result.append("Buzz")
             else:
-                list.append(i)
+                result.append(str(i))
 
-        return list
+        return result
+    
+if __name__ == "__main__":
+    n = int(input("Enter the number : "))
+    sol = Solution()
+    output = sol.fizzBuzz(n)
+    print(output)
